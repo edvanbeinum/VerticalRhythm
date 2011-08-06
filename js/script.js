ae90439e707c1c638e5343f0fb147d4f7a3aad3c
Ed van Beinum <ed@vanbeinum.org>
2011-08-07 00:48:46 +0100

    tidyup unused files

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
  */
 
 // add 978gs overlay
-jQuery(function() {
-	jQuery('div#gs-overlay').gsoverlay({
-		trigger: jQuery('a#gs-overlay-badge'),
+$(function() {
+	$('div#gs-overlay').gsoverlay({
+		trigger: $('a#gs-overlay-badge'),
 		grid: 978,
 		color: '#aa1b4a',
 		opacity: 0.15
